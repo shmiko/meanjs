c3ad7a42b3172f2a53296b6e35b0a29869848089
Paul Jones <shmiko@gmail.com>
2015-03-09 08:23:26 +1100

    ud

--- config/env/default.js
+++ config/env/default.js
@@ -5,7 +5,7 @@ module.exports = {
 	app: {
 		title: 'TripStomp',
 		description: 'Unravel Travel, from chore to more. Calendar,Mapping, Itineraries personalised the way you want it.',
-		keywords: 'MongoDB, Express, AngularJS, Node.js',
+		keywords: 'MongoDB, Express, AngularJS, Node.js and more',
 		googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
 	},
 	port: process.env.PORT || 3000,
