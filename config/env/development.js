40c2c01c07bb67bce0074a93adf5beaa75df167f
Paul Jones <shmiko@gmail.com>
2015-03-10 00:12:54 +1100

    name

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/meanestjs',
 	app: {
-		title: 'DEV TripStomp - UnRavel Travel'
+		title: 'TripStomp - UnRavel Travel'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
